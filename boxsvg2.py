4a5e4fd47cb19307a12b4f77857f915b687fc47e
Shane Gough <shane@thegaragelab.com>
2015-06-27 10:58:47 +1000

    More work on box cutter.

--- boxsvg2.py
+++ boxsvg2.py
@@ -42,6 +42,12 @@ PATH_TEMPLATE = """    <path
     />"""
 
 def mkLine(x1, y1, x2, y2):
+  x1 = round(x1, 4)
+  y1 = round(y1, 4)
+  x2 = round(x2, 4)
+  y2 = round(y2, 4)
+  if (x1 == x2) and (y1 == y2):
+    return None
   return Line(complex(x1, y1), complex(x2, y2))
 
 def createSVG(filename, tool, points):
@@ -53,12 +59,17 @@ def createSVG(filename, tool, points):
     allPoints.extend(point)
     path = Path()
     lx, ly = point[0]
+    pc = 0
     for p in point[1:]:
-      path.append(mkLine(lx, ly, p[0], p[1]))
+      l = mkLine(lx, ly, p[0], p[1])
+      if l is not None:
+        path.append(l)
+        pc = pc + 1
       lx = p[0]
       ly = p[1]
     path.closed = True
     paths.append(t.safe_substitute({ 'strokeWidth': tool, 'path': path.d() }))
+    print "Generated path with %d points." % pc
   # Build the final output
   v = { 
     'xmin': min([ x for x, y in allPoints ]) - (tool / 2),
@@ -78,37 +89,138 @@ def createSVG(filename, tool, points):
 # Geometry helpers
 #----------------------------------------------------------------------------
 
-def straightLine(length, tool):
-  for v in ( -tool / 2, length + (tool / 2)):
-    yield v
-    
-def horizontal(length, fixed, tool, reverse = False, generator = straightLine):
+def hLine(length, fixed, tool, reverse = False):
   points = list()
-  for x in generator(length, tool):
+  for x in ( -tool / 2, length + (tool / 2)):
     points.append((x, fixed))
   if reverse:
     points = list(reversed(points))
   return points
 
-def vertical(length, fixed, tool, reverse = False, generator = straightLine):
+def vLine(length, fixed, tool, reverse = False):
   points = list()
-  for y in generator(length, tool):
+  for y in ( -tool / 2, length + (tool / 2)):
     points.append((fixed, y))
   if reverse:
     points = list(reversed(points))
   return points
 
+def hTabs(length, fixed, tool, tabs, depth, reverse = False):
+  """ Generate a list containing the sequence of points for tabs
+  """
+  width = length / (tabs * 2)
+  pos = width / 2
+  points = [ (-tool / 2, fixed), ]
+  for p in range(tabs):
+    points.append((pos - (tool / 2), fixed))
+    points.append((pos - (tool / 2), fixed + depth))
+    pos = pos + width
+    points.append((pos + (tool / 2), fixed + depth))
+    points.append((pos + (tool / 2), fixed))
+    pos = pos + width
+  # Add the final point and return
+  points.append((length + tool / 2, fixed))
+  if reverse:
+    points = list(reversed(points))
+  return points
+
+def vTabs(length, fixed, tool, tabs, depth, reverse = False):
+  """ Generate a list containing the sequence of points for tabs
+  """
+  width = length / (tabs * 2)
+  pos = width / 2
+  points = [ (fixed, -tool / 2), ]
+  for p in range(tabs):
+    points.append((fixed, pos - (tool / 2)))
+    points.append((fixed + depth, pos - (tool / 2)))
+    pos = pos + width
+    points.append((fixed + depth, pos + (tool / 2)))
+    points.append((fixed, pos + (tool / 2)))
+    pos = pos + width
+  # Add the final point and return
+  points.append((fixed, length + tool / 2))
+  if reverse:
+    points = list(reversed(points))
+  return points
+
+def hSlots(length, fixed, tool, tabs, depth, reverse = False):
+  """ Generate a list containing the sequence of points for tabs
+  """
+  width = length / (tabs * 2)
+  pos = width / 2
+  points = [ (-tool / 2, fixed), (-tool / 2, fixed + depth), ]
+  for p in range(tabs):
+    points.append((pos + (tool / 2), fixed + depth))
+    points.append((pos + (tool / 2), fixed))
+    pos = pos + width
+    points.append((pos - (tool / 2), fixed))
+    points.append((pos - (tool / 2), fixed + depth))
+    pos = pos + width
+  # Add the final point and return
+  points.append((length + tool / 2, fixed + depth))
+  points.append((length + tool / 2, fixed))
+  if reverse:
+    points = list(reversed(points))
+  return points
+
+def vSlots(length, fixed, tool, tabs, depth, reverse = False):
+  """ Generate a list containing the sequence of points for tabs
+  """
+  width = length / (tabs * 2)
+  pos = width / 2
+  points = [ (fixed, -tool / 2), (fixed + depth, -tool / 2), ]
+  for p in range(tabs):
+    points.append((fixed + depth, pos + (tool / 2)))
+    points.append((fixed, pos + (tool / 2)))
+    pos = pos + width
+    points.append((fixed, pos - (tool / 2)))
+    points.append((fixed + depth, pos - (tool / 2)))
+    pos = pos + width
+  # Add the final point and return
+  points.append((fixed + depth, length + tool / 2))
+  points.append((fixed, length + tool / 2))
+  if reverse:
+    points = list(reversed(points))
+  return points
+  
 #----------------------------------------------------------------------------
 # Panel generators
 #----------------------------------------------------------------------------
 
 def generateBase(width, height, depth, tool, material, base_material):
   base = list()
-  base.extend(horizontal(width, -tool / 2, tool))
-  base.extend(vertical(height, width + (tool / 2), tool))
-  base.extend(horizontal(width, height + (tool / 2), tool, reverse = True))
-  base.extend(vertical(height, -tool / 2, tool, reverse = True))
+  base.extend(hTabs(width, -tool / 2, tool, 3, -material))
+  base.extend(vTabs(height, width + (tool / 2), tool, 1, material))
+  base.extend(hTabs(width, height + (tool / 2), tool, 3, material, reverse = True))
+  base.extend(vTabs(height, -tool / 2, tool, 1, -material, reverse = True))
   return [ base, ]
+ 
+def generateBack(width, height, depth, tool, material, base_material):
+  back = list()
+  back.extend(hLine(width, -tool / 2, tool))
+  back.extend(vLine(depth, width + (tool / 2), tool))
+  back.extend(hSlots(width, depth + (tool / 2), tool, 3, base_material, reverse = True))
+  back.extend(vLine(depth, -tool / 2, tool, reverse = True))
+  return [ back, ]
+
+def generateTop(width, height, depth, tool, material, base_material):
+  top = list()
+  top.extend(hLine(width, -tool / 2, tool))
+  top.extend(vTabs(height, width + (tool / 2), tool, 3, material))
+  top.extend(hLine(width, height + (tool / 2), tool, reverse = True))
+  top.extend(vTabs(height, -tool / 2, tool, 3, -material, reverse = True))
+  return [ top, ]
+
+def generateSide(width, height, depth, tool, material, base_material):
+  side = list()
+#  side.extend(hLine(height, -tool / 2, tool))
+  side.extend(hSlots(height, -tool / 2, tool, 3, -material))
+  side.extend(vLine(depth, height + (tool / 2), tool))
+#  side.extend(hLine(height, depth + (tool / 2), tool, reverse = True))
+  side.extend(hSlots(height, depth + (tool / 2), tool, 1, base_material, reverse = True))
+  side.extend(vLine(depth, -tool / 2, tool, reverse = True))
+  print side
+  return [ side, ]
   
 #----------------------------------------------------------------------------
 # Main program
@@ -142,6 +254,10 @@ if __name__ == "__main__":
     basematerial = material
   else:
     basematerial = options.basematerial
-  # Generate the panels
-  createSVG(prefix + "base.svg", options.tool, generateBase(options.width, options.height, options.depth, options.tool, material, basematerial))
+  # Generate the panels for the bottom
+  createSVG(prefix + "base.svg", options.tool, generateBase(options.width + (2 * options.material), options.height, options.depth, options.tool, material, basematerial))
+  createSVG(prefix + "back.svg", options.tool, generateBack(options.width + (2 * options.material), options.height, options.depth + options.material, options.tool, material, basematerial))
+  # Generate the panels for the top
+  createSVG(prefix + "top.svg", options.tool, generateTop(options.width + (2 * options.material), options.height, options.depth, options.tool, material, basematerial))
+  createSVG(prefix + "side.svg", options.tool, generateSide(options.width + (2 * options.material), options.height, options.depth, options.tool, material, basematerial))
   
