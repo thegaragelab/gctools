aca829fa4d86da59a83c34506f45b601ef6d441a
Shane Gough <shane@thegaragelab.com>
2015-07-21 11:11:38 +1000

    Working on loading boards, setting up panel and initial size checks.

--- pcbpack.py
+++ pcbpack.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 #----------------------------------------------------------------------------
+# 21-Jul-2015 ShaneG
+#
+# Major refactoring and updates to make it part of the 'gctools' suite.
+#
 # 13-Nov-2014 ShaneG
 #
 # Some slight changes to the code for consistancy with the output generated
@@ -15,65 +19,64 @@
 #
 # Tool to pack multiple PCB g-code files into a single panel.
 #----------------------------------------------------------------------------
+from util import LOG, fromJSONFile
+from random import randint
+from os.path import realpath, splitext, exists
+from optparse import OptionParser
 
-def logDebug(msg):
-  #print "DEBUG: %s" % msg
-  pass
-
-def logError(msg):
-  print "ERROR: %s" % msg
-
-
+#--- Globals
+CONFIG = None
 
 #----------------------------------------------------------------------------
-# Class wrapper to manage boards
+# Manage board positioning
+#
+# The width and height of the board must include any spacing required and
+# will be taken into account when modifying the gcode.
 #----------------------------------------------------------------------------
 
-class Board:
+class BoardPosition:
   """ Represents a board.
 
     Each board has a fixed size (width and height), a mutable location and
     a rotation flag.
   """
 
-  def __init__(self, width, height, name = "", xoff = 0.0, yoff = 0.0):
+  def __init__(self, name, w, h):
     """ Constructor with dimensions
     """
     self.name = name
-    self._width = width
-    self._height = height
-    self._xoff = xoff
-    self._yoff = yoff
+    self._width = w
+    self._height = h
     self.reset()
 
   def __str__(self):
-    return "Board %0.2f x %0.2f @ %0.2f, %0.2f (Rot = %s)" % (self.width, self.height, self.x, self.y, self.rotated)
+    return "Board %0.2f x %0.2f @ %0.2f, %0.2f (Rot = %s)" % (self.w, self.h, self.x, self.y, self.rotated)
 
   @property
-  def width(self):
+  def w(self):
     if self.rotated:
       return self._height
     return self._width
 
-  @width.setter
-  def width(self, value):
-    raise Exception("Cannot modify 'width' after creation")
+  @w.setter
+  def w(self, value):
+    raise Exception("Cannot modify width after creation")
 
   @property
-  def height(self):
+  def h(self):
     if self.rotated:
       return self._width
     return self._height
 
-  @height.setter
-  def height(self, value):
-    raise Exception("Cannot modify 'height' after creation")
+  @h.setter
+  def h(self, value):
+    raise Exception("Cannot modify height after creation")
 
   def reset(self):
     """ Restore to original (unrotated, untranslated) state
     """
-    self.x = 0
-    self.y = 0
+    self.x = 0.0
+    self.y = 0.0
     self.rotated = False
 
   def translate(self, dx, dy):
@@ -101,57 +104,67 @@ class Board:
     """
     return self.width * self.height
 
-  def clone(self, dw = 0, dh = 0):
-    return Board(self._width + dw, self._height + dh, self.name, self._xoff, self._yoff)
+  def clone(self):
+    copy = BoardPosition(self.name, self.w, self.h)
+    copy.x = self.x
+    copy.y = self.y
+    return copy
 
-  def getAdjustedCode(self, filetype):
-    """ Apply the current translation to the code
-    """
-    data = None
-    if filetype <> 1:
-      with open(getCodeFile(self.name, filetype), "r") as f:
-        data = f.readlines()
-# TODO: Edgemill files need to be flipped. For now just use rectangles
-#      if filetype == 2:
-#        # Edgemill files need to be flipped in X
-#        data = runTool(GRECODE, "-xflip", data)
-#        x, y, w, h = getBoardSize(self.name)
-#        print "DEBUG: Edgemill (raw)"
-#        print self.name
-#        print x, y, w, h
-#        data = runTool(GRECODE, "-shift %0.2f 0.0" % w, data)
-    else:
-      # Drill files get generated, not loaded
-      data = generateDrillFile(self.name)
-    # Determine the translation we need
-    source = ( self._xoff, self._yoff, self._xoff + self._width, self._yoff + self._height )
-    if self.rotated:
-      target = ( self.x + self.width, self.y, self.x, self.y + self.height )
-    else:
-      target = ( self.x, self.y, self.x + self.width, self.y + self.height)
-    # Do the translation/rotation
-    data = runTool(GRECODE, "-overlay %s %s" % ("%0.2f %0.2f %0.2f %0.2f" % source, "%0.2f %0.2f %0.2f %0.2f" % target), data)
-    return data
+#----------------------------------------------------------------------------
+# Manage panel layout
+#
+# A panel has a fixed size and (optionally) a set of fixed zones that cannot
+# be used (mounting screws for example).
+#----------------------------------------------------------------------------
 
-def loadBoard(name):
-  """ Create a board instance from a file
+class Panel:
+  """ Represents a single panel
   """
-  if not checkFiles(name):
-    raise Exception("Missing required g-code file for board '%s'" % self.name)
-  # Get the board dimensions and create the instance
-  xoff, yoff, width, height = getBoardSize(name)
-  return Board(width + PANEL_SPACING, height + PANEL_SPACING, name, xoff, yoff)
 
-#----------------------------------------------------------------------------
-# Generate an image of the layout
-#----------------------------------------------------------------------------
+  def __init__(self, name):
+    """ Create the panel from the named configuration
+    """
+    global CONFIG
+    # Set up state
+    self.w = CONFIG['panels'][name]['width']
+    self.h = CONFIG['panels'][name]['height']
+    self.padding = CONFIG['panels'][name].get('padding', 2)
+    self.description = CONFIG['panels'][name].get('description', "(undefined)")
+    self.locked = list()
+    if CONFIG['panels'][name].has_key("locked"):
+      for lockInfo in CONFIG['panels'][name]['locked']:
+        lock = BoardPosition("_lock_", lockInfo['w'], lockInfo['h'])
+        lock.x = lockInfo['x']
+        lock.y = lockInfo['y']
 
-def createLayoutImage(panel, boards, filename):
-  img = Image.new("RGB", (int(panel.width * 2), int(panel.height * 2)), "white")
-  drw = ImageDraw.Draw(img)
-  for board in boards:
-    drw.rectangle((int(board.x * 2), int(board.y * 2), int((board.x + board.width) * 2), int((board.y + board.height) * 2)), fill = "black")
-  img.save(filename)
+  def willFit(self, board):
+    """ Determine if the board will fit
+    """
+    if (board.w > (self.w - (2 * self.padding))) and (board.w > (self.h - (2 * self.padding))):
+      return False
+    if (board.h > (self.w - (2 * self.padding))) and (board.h > (self.h - (2 * self.padding))):
+      return False
+    return True
+
+  #--------------------------------------------------------------------------
+  # Utility methods
+  #--------------------------------------------------------------------------
+
+  def createImage(self, filename):
+    """ Generate an image for the current layout.
+    """
+    pass
+    """ TODO: Reimplement this
+
+    img = Image.new("RGB", (int(panel.width * 2), int(panel.height * 2)), "white")
+    drw = ImageDraw.Draw(img)
+    for board in boards:
+      drw.rectangle((int(board.x * 2), int(board.y * 2), int((board.x + board.width) * 2), int((board.y + board.height) * 2)), fill = "black")
+    img.save(filename)
+    """
+
+  def __str__(self):
+    return "%s - %0.1f x %0.1f mm" % (self.description, self.w, self.h)
 
 #----------------------------------------------------------------------------
 # Layout operations
@@ -224,10 +237,71 @@ def consumedArea(boards):
   return max([ b.x + b.width for b in boards]) * max([ b.y + b.height for b in boards])
 
 #----------------------------------------------------------------------------
+# Helpers
+#----------------------------------------------------------------------------
+
+BOARD_CACHE = dict()
+
+def loadBoard(name):
+  """ Load the named board from the repository
+  """
+  # TODO: Implement this. For now we generate a random board if we haven't
+  #       seen the name before
+  global BOARD_CACHE
+  board = BOARD_CACHE.get(name, None)
+  if board is None:
+    # Create a new one and cache it
+    board = BoardPosition(
+      name,
+      15 + randint(0, 200),
+      15 + randint(0, 200)
+      )
+    LOG.DEBUG(str(board))
+    BOARD_CACHE[name] = board
+  # Done
+  return board
+
+#----------------------------------------------------------------------------
 # Main program
 #----------------------------------------------------------------------------
 
 if __name__ == "__main__":
+  # Load the configuration
+  cfg = splitext(realpath(__file__))[0] + ".json"
+  if not exists(cfg):
+    LOG.FATAL("Could not find configuration file '%s'" % cfg)
+  try:
+    CONFIG = fromJSONFile(cfg)
+  except Exception, ex:
+    LOG.FATAL("Could not load configuration file '%s' - %s" % (cfg, ex))
+  # Process command line arguments
+  parser = OptionParser()
+  parser.add_option("-n", "--no-optimise", action="store_false", default=True, dest="optimise")
+  parser.add_option("-o", "--output", action="store", type="string", dest="output")
+  parser.add_option("-p", "--panel", action="store", type="string", dest="panel")
+  options, args = parser.parse_args()
+  # Check for required options
+  for required in ("output", "panel"):
+    if getattr(options, required) is None:
+      LOG.FATAL("Missing required option '%s'" % required)
+  # Set up the panel
+  try:
+    panel = Panel(options.panel)
+  except Exception, ex:
+    LOG.FATAL("Could not load panel defintion '%s'" % options.panel)
+  LOG.DEBUG("Panel - %s" % panel)
+  # Load boards
+  boards = list()
+  for name in args:
+    board = loadBoard(name)
+    if board is None:
+      LOG.FATAL("Could not find board '%s' in repository" % name)
+    if not panel.willFit(board):
+      LOG.FATAL("Board %s will not fit on this panel" % name)
+    boards.append(board)
+  if len(boards) == 0:
+    LOG.FATAL("No boards specified on command line")
+"""
   # Process command line
   boards = list()
   index = 1
@@ -316,3 +390,4 @@ if __name__ == "__main__":
       for line in lines:
         gcode.write(line)
   print "Operation complete."
+"""
