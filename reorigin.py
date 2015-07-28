d7f805550f68fedea58f0b47815b9ede350ac74c
Shane Gough <shane@thegaragelab.com>
2015-07-28 13:59:39 +1000

    Updated for new framework (reorigin.py)

--- reorigin.py
+++ reorigin.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 #----------------------------------------------------------------------------
+# 28-Jul-2015 ShaneG
+#
+# Updated to use the new framework.
+#
 # 04-Dec-2014 ShaneG
 #
 # A simple program to reset the origin of the work.
 #----------------------------------------------------------------------------
 from sys import argv
 from optparse import OptionParser
-from gcode import Bounds, Translate, loadGCode, saveGCode, floatVal
+from os.path import splitext
+from util import loadGCode, saveGCode, Translate
 
 #--- Usage information
 USAGE = """
 Usage:
-       %s [--output filename] filename
+       %s [--image] [--output filename] filename
 """
 
 #--- Main program
@@ -19,22 +24,26 @@ if __name__ == "__main__":
   # Set up program options
   parser = OptionParser()
   parser.add_option("-o", "--output", action="store", type="string", dest="output_file")
+  parser.add_option("-i", "--image", action="store_true", dest="image", default=False)
   options, args = parser.parse_args()
+  # Check for required options
+  for required in ("output_file", ):
+    if getattr(options, required, None) is None:
+      print "Missing required option '%s'" % required
+      print USAGE.strip() % argv[0]
+      exit(1)
   # Check positional arguments
   if len(args) <> 1:
     print USAGE.strip() % argv[0]
     exit(1)
-  # Set up the source file and collect the depth information
-  source = args[0]
-  # First get the bounds of the file and determine the translation
-  data = loadGCode(source)
-  bounds = Bounds()
-  bounds.apply(data)
-  # Now translate so that minx and miny become 0
-  print "Translating by X = %s, Y = %s" % (floatVal(-bounds.minx), floatVal(-bounds.miny))
-  translate = Translate(dx = -bounds.minx, dy = -bounds.miny)
-  result = translate.apply(data)
-  if options.output_file is None:
-    print "\n".join(result)
+  # Process the file
+  gcode = loadGCode(args[0])
+  if (gcode.minx <> 0) or (gcode.miny <> 0):
+    print "Translating file by dx = %0.4f, dy = %0.4f" % (-gcode.minx, -gcode.miny)
+    gcode = gcode.clone(Translate(-gcode.minx, -gcode.miny))
   else:
-    saveGCode(options.output_file, result)
+    print "No translation required."
+  saveGCode(options.output_file, gcode)
+  if options.image:
+    gcode.render(splitext(options.output_file)[0] + ".png")
+
