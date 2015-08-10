4c8b1089f6b0229d86fcb974d51891f6aa29b08d
Shane Gough <shane@thegaragelab.com>
2015-08-10 19:19:08 +1000

    Updated optimisation algorithm, it seems to be working ok.

--- util/optimise.py
+++ util/optimise.py
@@ -22,6 +22,12 @@ class Point:
     self.x = x
     self.y = y
     
+  def distanceFrom(self, x, y):
+    return distance(self.x, self.y, x, y)
+    
+  def generate(self, gcode, feed):
+    return self.x, self.y
+    
 class Line(Point):
   """ Represents a line
   """
@@ -30,7 +36,11 @@ class Line(Point):
     Point.__init__(self, x, y)
     self.tx = tx
     self.ty = ty
-    
+
+  def generate(self, gcode, feed):
+    gcode.append("G01 X%0.4f Y%0.4f F%0.4f" % (self.tx, self.ty, feed))
+    return self.tx, self.ty
+
 class Arc(Line):
   """ Represents an arc
   """
@@ -40,13 +50,19 @@ class Arc(Line):
     self.cx = cx
     self.cy = cy
     self.cmd = cmd
-    
+
+  def generate(self, gcode, feed):
+    gcode.append("%s X%0.4f Y%0.4f I%0.4f J%0.4f F%0.4f" % (self.cmd, self.tx, self.ty, self.cx, self.cy, feed))
+    return self.tx, self.ty
+
 def optimise(source):
   """ Return an optimised copy of the given gcode
   """
   # Build up a sequence of cutting operations
   x, y, z = 0.0, 0.0, 0.0
   cut, safe = source.minz, source.maxz
+  insert_feed = 250
+  feed = 500
   prefix = list()
   movements = list()
   airtime = 0.0
@@ -60,6 +76,7 @@ def optimise(source):
       z = nz
       insert = True
       cutting = True
+      insert_feed = cmd.F or insert_feed
       continue
     if (nz >= 0.0) and (z < 0.0):
       z = nz
@@ -76,14 +93,46 @@ def optimise(source):
       if cmd.command == "G01":
         # Line
         movements.append(Line(x, y, nx, ny))
+        feed = cmd.F or feed
       elif cmd.command in ("G02", "G03"):
         # Arc
         movements.append(Arc(x, y, nx, ny, cmd.I, cmd.J, cmd.command))
+        feed = cmd.F or feed
       insert = False
     else:
       airtime = airtime + distance(x, y, nx, ny)
     # Update position
     x, y = nx, ny
   LOG.INFO("    Original - %d operations, %dmm air travel" % (len(movements), int(airtime)))
-  return source.clone()
-
+  if len(movements) == 0:
+    LOG.INFO("    No optimisation can be performed.")
+    return source
+  # Now generate an optimised order of operations
+  x, y, nair = 0.0, 0.0, 0.0
+  first = True
+  optimised = GCode()
+  while len(movements) > 0:
+    # Sort by distance to current point
+    movements.sort(cmp = lambda a, b: cmp(a.distanceFrom(x, y), b.distanceFrom(x, y)))
+    current = movements[0]
+    movements = movements[1:]
+    # Do we need to do a retraction and insertion ?
+    if first or (x <> current.x) or (y <> current.y):
+      if not first:
+        # Retract
+        optimised.append("G00 Z%0.4f" % safe)
+      first = False
+      if (x <> current.x) or (y <> current.y):
+        # Move to co-ordinate
+        optimised.append("G00 X%0.4f Y%0.4f" % (current.x, current.y))
+        nair = nair + distance(x, y, current.x, current.y)
+        x, y = current.x, current.y
+      # Insert
+      optimised.append("G01 Z%0.4f F%0.4f" % (cut, insert_feed))
+    # Do the movement
+    x, y = current.generate(optimised, feed)
+  # Retract
+  optimised.append("G00 Z%0.4f" % safe)
+  # See what we came up with
+  LOG.INFO("    Optimised - %dmm air travel, %d %% of original." % (int(nair), int((100.0 * nair) / airtime)))
+  return optimised
